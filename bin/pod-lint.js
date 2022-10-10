658457464e7e8bc5ea4ae7d0186634c76404cc4d
Soumya Ranjan Mahunt <soumya.mahunt@gmail.com>
2022-10-10 22:50:29 +0530

    feat: add multi podspec linting

--- bin/pod-lint.js
+++ bin/pod-lint.js
@@ -1,19 +1,36 @@
 #!/usr/bin/env node
 const { execSync } = require('child_process');
+const readdirGlob = require('readdir-glob');
 const core = require('@actions/core');
 
-
-const args = process.argv.slice(2).join(' ');
-const command = `pod lib lint ${args} \
-  --no-clean \
-  --allow-warnings \
-  --verbose`;
-
-core.startGroup(`Linting podspec`);
-core.info(`Running command: ${command}`);
-execSync(command, {
-    stdio: ['inherit', 'inherit', 'inherit'],
-    encoding: 'utf-8'
+let podspecs = [];
+const specGlobberer = readdirGlob('.', { pattern: '*.podspec' });
+specGlobberer.on(
+  'match',
+  m => {
+    podspecs.push(m.relative);
   }
 );
-core.endGroup();
+
+specGlobberer.on(
+  'end',
+  () => {
+    const args = process.argv.slice(2).join(' ');
+    const command = `pod lib lint ${args} \
+      --no-clean \
+      --allow-warnings \
+      --verbose \
+      --include-podspecs=\\{${podspecs.join(',')}\\}`;
+    
+    core.startGroup(`Linting podspec`);
+    core.info(`Running command: ${command}`);
+    execSync(command, {
+        stdio: ['inherit', 'inherit', 'inherit'],
+        encoding: 'utf-8'
+      }
+    );
+    core.endGroup();
+  }
+);
+
+specGlobberer.on('error', err => { core.error(err); });
